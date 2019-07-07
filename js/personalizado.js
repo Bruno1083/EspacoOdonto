590165eaa6b538c06894926ceaf8491fa912a890
Bruno Cavalcante <bruno1083@hotmail.com>
2019-07-07 20:48:39 -0300

    Animações

---
+++ js/personalizado.js
@@ -0,0 +1,21 @@
+window.sr = ScrollReveal({reset: true});
+sr.reveal('.anim_left', {
+    duration:2000,
+    origin: 'left',
+    distance: '20px'
+});
+sr.reveal('.anim_bottom', {
+    duration:2000,
+    origin: 'bottom',
+    distance: '20px'
+});
+sr.reveal('.anim_right', {
+    duration:2000,
+    origin: 'right',
+    distance: '20px'
+});
+sr.reveal('.anim_top', {
+    duration:2000,
+    origin: 'top',
+    distance: '20px'
+});
